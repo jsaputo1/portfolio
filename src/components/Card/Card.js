b2e374a536cb94e7cfdd61e82fba9ba18422368a
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-17 23:13:47 -0400

    HTML shell for card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,6 +1,18 @@
-import React from "react";
-function Title() {
-  return <h1>This is the Title</h1>;
+import React from "./node_modules/react";
+import "./card.scss";
+function Card(props) {
+  return <div className="card-container">
+    <figure className="card-image">
+      <img src="card" alt="website-preview" />
+      <div className="card-second-row">
+        <h3 className="card-title">Title</h3>
+        <div className="website-links">
+          <span>Link</span>
+          <span>Link 2</span>
+        </div>
+      </div>
+    </figure>
+  </div>;
 }
 
-export default Title;
+export default Card;
