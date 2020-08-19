5e4d905c4680a7420e3e86a1666d1a9087895cb3
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-19 19:32:56 -0400

    changed information to props

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,22 +4,20 @@ import "./Card.scss";
 function Card(props) {
   return <div className="card-container">
     <figure className="card-figure">
-      <a href="demo" target="new">
-        <img src="/images/thumbnails/qod.png" alt="website-preview" />
+      <a href={props.demoLink} target="new">
+        <img src={props.thumbnail} alt="website-preview" />
       </a>
       <div className="card-second-row">
-        <h3 className="card-title">Quotes on Dev</h3>
+        <h3 className="card-title">{props.title}</h3>
         <div className="website-links">
-          <span><a href="github" target="new"><i class="fab fa-github"></i></a></span>
-          <span><a href="demo" target="new"><i class="fas fa-external-link-alt"></i></a></span>
+          <span><a href={props.githubLink} target="new"><i class="fab fa-github"></i></a></span>
+          <span><a href={props.demoLink} target="new"><i class="fas fa-external-link-alt"></i></a></span>
         </div>
       </div>
       <div className="card-description">
         <h3 className="card-description-title">Description</h3>
         <hr />
-        <h4>Quotes on Dev is a multi page web app that shows random developer themed quotes, and also allows registered
-        users to submit their own quotes. The app uses Word Press as the content management system, and is fully
-           responsive with breakpoints for Tablet and Mobile.</h4>
+        <h4>{props.description}</h4>
       </div>
     </figure>
   </div>;
--- src/components/Card/Card.stories.js
+++ src/components/Card/Card.stories.js
@@ -6,8 +6,24 @@ export default {
   component: Card,
 };
 
-export const Default = () => <Card />;
+
+const cardObject = {
+  title: "Quotes on Dev",
+  demoLink: "https://www.johnsaputo.net/qod",
+  githubLink: "https://github.com/jsaputo1/quotesondev",
+  thumbnail: "http://localhost:6006/images/thumbnails/qod.png",
+  description: "Quotes on Dev is a multi page web app that shows random developer themed quotes, and also allows registered users to submit their own quotes. The app uses Word Press as the content management system, and is fully responsive with breakpoints for Tablet and Mobile."
+};
+
+export const Default = () => <Card
+  title={cardObject.title}
+  demoLink={cardObject.demoLink}
+  githubLink={cardObject.githubLink}
+  thumbnail={cardObject.thumbnail}
+  description={cardObject.description}
+
+/>;
 
 Default.story = {
-  name: "default",
+  name: "Default",
 };
