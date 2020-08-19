95cbcf8802d617d0512766f5ecf5c6df1ebf6add
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-19 19:48:21 -0400

    created initial project data object, exported

---
+++ src/data/projects.js
@@ -0,0 +1,21 @@
+const projectDataObject = [
+  {
+    id: 1,
+    title: "Quotes on Dev",
+    demoLink: "https://www.johnsaputo.net/qod",
+    githubLink: "https://github.com/jsaputo1/quotesondev",
+    thumbnail: "http://localhost:6006/images/thumbnails/qod.png",
+    description: "Quotes on Dev is a multi page web app that shows random developer themed quotes, and also allows registered users to submit their own quotes. The app uses Word Press as the content management system, and is fully responsive with breakpoints for Tablet and Mobile."
+  },
+  {
+    id: 2,
+    title: "Test",
+    demoLink: "https://www.johnsaputo.net/qod",
+    githubLink: "https://github.com/jsaputo1/quotesondev",
+    thumbnail: "http://localhost:6006/images/thumbnails/qod.png",
+    description: "Test"
+  }
+
+];
+
+module.exports = { projectDataObject };
