c389dce556cad2c85d9c4d397a710679764b2032
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-19 22:16:35 -0400

    removed object data from card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,42 +1,25 @@
 import React from "react";
 import "./Card.scss";
-import projectDataObject from "../../data/projects";
 
 function Card(props) {
-  let jsxData = [];
-  for (let key in projectDataObject) {
-    let newObj = projectDataObject[key];
-    for (let key in newObj) {
-      let newObj2 = newObj[key];
-      for (let key in newObj2) {
-        let data = (newObj2[key]);
-        let jsxContent = (
-          <div className="card-wrapper">
-            <figure className="card-figure">
-              <a href={data.demoLink} target="new">
-                <img src={data.thumbnail} alt="website-preview" />
-              </a>
-              <div className="card-second-row">
-                <h3 className="card-title">{data.title}</h3>
-                <div className="website-links">
-                  <span><a href={data.githubLink} target="new"><i class="fab fa-github"></i></a></span>
-                  <span><a href={data.demoLink} target="new"><i class="fas fa-external-link-alt"></i></a></span>
-                </div>
-              </div>
-              <div className="card-description">
-                <h3 className="card-description-title">Description</h3>
-                <hr />
-                <h4>{data.description}</h4>
-              </div>
-            </figure>
-          </div>
-        );
-        jsxData.push(jsxContent);
-      }
-    }
-  }
   return <div className="card-container">
-    {jsxData}
+    <figure className="card-figure">
+      <a href={props.demoLink} target="new">
+        <img src={props.thumbnail} alt="website-preview" />
+      </a>
+      <div className="card-second-row">
+        <h3 className="card-title">{props.title}</h3>
+        <div className="website-links">
+          <span><a href={props.githubLink} target="new"><i class="fab fa-github"></i></a></span>
+          <span><a href={props.demoLink} target="new"><i class="fas fa-external-link-alt"></i></a></span>
+        </div>
+      </div>
+      <div className="card-description">
+        <h3 className="card-description-title">Description</h3>
+        <hr />
+        <h4>{props.description}</h4>
+      </div>
+    </figure>
   </div>;
 }
 
