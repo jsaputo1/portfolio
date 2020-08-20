c48ba3d661b7dcd1b1cd41f0efdb2f17b339abf6
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-19 22:41:00 -0400

    added data for aloha project

--- src/data/projects.js
+++ src/data/projects.js
@@ -9,13 +9,53 @@ const frontEndProjects = {
     },
 
     s2: {
-      title: "Test",
-      demoLink: "https://www.johnsaputo.net/qod",
-      githubLink: "https://github.com/jsaputo1/quotesondev",
-      thumbnail: "http://localhost:6006/images/thumbnails/qod.png",
-      description: "Test"
+      title: "Aloha",
+      demoLink: "https://www.johnsaputo.net/aloha",
+      githubLink: "https://github.com/jsaputo1/aloha",
+      thumbnail: "http://localhost:6006/images/thumbnails/aloha.png",
+      description: "Aloha is a single page design of an online clothing store. It was designed mobile first with responsive views for Tablet and Desktop."
     }
 
 };
 
-module.exports = { frontEndProjects };
+const fullStackProjects = {
+
+  s1: {
+    title: "Quotes on Dev",
+    demoLink: "https://www.johnsaputo.net/qod",
+    githubLink: "https://github.com/jsaputo1/quotesondev",
+    thumbnail: "http://localhost:6006/images/thumbnails/qod.png",
+    description: "Quotes on Dev is a multi page web app that shows random developer themed quotes, and also allows registered users to submit their own quotes. The app uses Word Press as the content management system, and is fully responsive with breakpoints for Tablet and Mobile."
+  },
+
+  s2: {
+    title: "Test",
+    demoLink: "https://www.johnsaputo.net/qod",
+    githubLink: "https://github.com/jsaputo1/quotesondev",
+    thumbnail: "http://localhost:6006/images/thumbnails/qod.png",
+    description: "Test"
+  }
+
+};
+
+const wordPressProjects = {
+
+  s1: {
+    title: "Quotes on Dev",
+    demoLink: "https://www.johnsaputo.net/qod",
+    githubLink: "https://github.com/jsaputo1/quotesondev",
+    thumbnail: "http://localhost:6006/images/thumbnails/qod.png",
+    description: "Quotes on Dev is a multi page web app that shows random developer themed quotes, and also allows registered users to submit their own quotes. The app uses Word Press as the content management system, and is fully responsive with breakpoints for Tablet and Mobile."
+  },
+
+  s2: {
+    title: "Test",
+    demoLink: "https://www.johnsaputo.net/qod",
+    githubLink: "https://github.com/jsaputo1/quotesondev",
+    thumbnail: "http://localhost:6006/images/thumbnails/qod.png",
+    description: "Test"
+  }
+
+};
+
+module.exports = { frontEndProjects};
