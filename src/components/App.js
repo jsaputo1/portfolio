528e049ca5a1b9657eb12b20094b5cf853d785df
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-19 21:46:28 -0400

    moved card out of app header

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import "./App.scss"
+import "./App.scss";
 import Card from "./Card/Card";
+import projectDataObject from "../data/projects";
+import projectDataArray from "../data/projects";
+// console.log(projectDataObject);
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Card></Card>
       </header>
+      <Card></Card>
     </div>
   );
-}
 
+
+
+}
 export default App;
 
