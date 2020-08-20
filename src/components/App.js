a71e082ab66032c6438667e76fbfe3948395b6ab
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-19 22:17:30 -0400

    added map to loop through project data

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,32 @@ import React from 'react';
 import "./App.scss";
 import Card from "./Card/Card";
 import projectDataObject from "../data/projects";
-import projectDataArray from "../data/projects";
-// console.log(projectDataObject);
-
-
 
 function App() {
+
+  let data = {};
+  for (let key in projectDataObject) {
+    let newObj = projectDataObject[key];
+    for (let key in newObj) {
+      data = newObj[key];
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
       </header>
-      <Card></Card>
+      {Object.keys(data).map(function (key) {
+        return <Card
+          title={data[key].title}
+          demoLink={data[key].demoLink}
+          githubLink={data[key].githubLink}
+          thumbnail={data[key].thumbnail}
+          description={data[key].description}
+        />;
+      })}
     </div>
   );
-
-
 
 }
 export default App;
