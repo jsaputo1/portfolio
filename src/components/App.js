a51522d90a8b4a81a120eb55a508faa3de068ba2
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-19 22:36:22 -0400

    cleaned up JSX code. Added and commented out code for backend/wordpress jsx

--- src/components/App.js
+++ src/components/App.js
@@ -2,27 +2,71 @@ import React from 'react';
 import "./App.scss";
 import Card from "./Card/Card";
 import frontEndProjects from "../data/projects";
+// import fullStackProjects from "../data/projects";
+// import wordPressProjects from "../data/projects";
 
 function App() {
 
-  let data = {};
+  //Front End Code
+  let frontEndData = {};
   for (let key in frontEndProjects) {
-    data = frontEndProjects[key];
+    frontEndData = frontEndProjects[key];
   }
+  const frontEndJsx =
+    Object.keys(frontEndData).map(function (key) {
+      return <Card
+        title={frontEndData[key].title}
+        demoLink={frontEndData[key].demoLink}
+        githubLink={frontEndData[key].githubLink}
+        thumbnail={frontEndData[key].thumbnail}
+        description={frontEndData[key].description}
+      />;
+    });
+
+  // let backEndData = {};
+  // for (let key in backEndProjects) {
+  //   backEndData = backEndProjects[key];
+  // }
+  // const backEndJsx =
+  //   Object.keys(frontEndData).map(function (key) {
+  //     return <Card
+  //       title={frontEndData[key].title}
+  //       demoLink={frontEndData[key].demoLink}
+  //       githubLink={frontEndData[key].githubLink}
+  //       thumbnail={frontEndData[key].thumbnail}
+  //       description={frontEndData[key].description}
+  //     />;
+  //   })
+
+  // let wordPressData = {};
+  // for (let key in wordPressProjects) {
+  //   wordPressData = wordPressProjects[key];
+  // }
+  // const wordPressJsx =
+  //   Object.keys(frontEndData).map(function (key) {
+  //     return <Card
+  //       title={frontEndData[key].title}
+  //       demoLink={frontEndData[key].demoLink}
+  //       githubLink={frontEndData[key].githubLink}
+  //       thumbnail={frontEndData[key].thumbnail}
+  //       description={frontEndData[key].description}
+  //     />;
+  //   });
+
 
   return (
     <div className="App">
       <header className="App-header">
       </header>
-      {Object.keys(data).map(function (key) {
-        return <Card
-          title={data[key].title}
-          demoLink={data[key].demoLink}
-          githubLink={data[key].githubLink}
-          thumbnail={data[key].thumbnail}
-          description={data[key].description}
-        />;
-      })}
+      <section class="front-end-projects">
+        {frontEndJsx}
+      </section>
+      <section class="back-end-projects">
+        {/* {backEndJsx} */}
+      </section>
+      <section class="wordPress-projects">
+        {/* {wordPressJsx} */}
+      </section>
     </div>
   );
 
