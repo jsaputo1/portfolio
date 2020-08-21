6865c8553a2258441feb1d9c05e7a2b0d1f7013b
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-21 19:42:43 -0400

    filled details into header shell

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,17 +2,16 @@ import React from "react";
 import "./Header.scss";
 
 function Header(props) {
-  return <header>
+  return <header className="header-container">
     <div className="left-side-header">
       <h3>John Saputo</h3>
     </div>
     <div className="right-side-header">
-      <span>Link</span>
-      <span>Link</span>
-      <span>Link</span>
+      <span><a href={props.githubLink} target="new"><i class="fab fa-github"></i></a></span>
+      <span><a href={props.email} target="new"><i class="fas fa-envelope-square"></i></a></span>
     </div>
-
   </header>;
 }
 
 export default Header;
+
