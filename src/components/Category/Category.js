6fc55744139137175be2edc274f8b66ef00fc625
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-22 14:04:19 -0400

    created files for category component

---
+++ src/components/Category/Category.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Category.scss";
+
+function Category(props) {
+  return <div></div>;
+}
+
+export default Category;
+
---
+++ src/components/Category/Category.stories.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Category from "./Category";
+
+export default {
+  title: "Category",
+  component: Category,
+};
+
+export const Default = () => <Category
+
+/>;
+
+Default.story = {
+  name: "Default",
+};
