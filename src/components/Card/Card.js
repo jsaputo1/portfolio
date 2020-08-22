8dd054985161bbe4d44f8a00357276271c181ace
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-21 20:30:47 -0400

    added card-header div, moved title to card-header

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,20 +3,21 @@ import "./Card.scss";
 
 function Card(props) {
   return <div className="card-container">
+    <div className="card-header">
+      <h3 className="card-title">{props.title}</h3>
+    </div>
     <figure className="card-figure">
       <a href={props.demoLink} target="new">
         <img src={props.thumbnail} alt="website-preview" />
       </a>
-      <div className="card-second-row">
-        <h3 className="card-title">{props.title}</h3>
+      <div className="card-description-header">
+        <h3>Description</h3>
         <div className="website-links">
           <span><a href={props.githubLink} target="new"><i class="fab fa-github"></i></a></span>
           <span><a href={props.demoLink} target="new"><i class="fas fa-external-link-alt"></i></a></span>
         </div>
       </div>
       <div className="card-description">
-        <h3 className="card-description-title">Description</h3>
-        <hr />
         <h4>{props.description}</h4>
       </div>
     </figure>
