460c22e2f5255f03319997288fba58f5e95e7004
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-23 14:43:33 -0400

    modified code for project sections

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,10 @@ import "./App.scss";
 import Header from "./Header/Header";
 import Card from "./Card/Card";
 import Category from "./Category/Category";
-import frontEndProjects from "../data/projects";
-import fullStackProjects from "../data/projects";
-import wordPressProjects from "../data/projects";
-import retrieveData from "../helpers/retrieveData";
+// import { frontEndProjects, fullStackProjects, wordPressProjects}  from "../data/projects";
+const { frontEndProjects } = require("../data/projects");
+const { fullStackProjects } = require("../data/projects");
+const { wordPressProjects } = require("../data/projects");
 
 
 function App() {
@@ -14,8 +14,10 @@ function App() {
   //Set state for portfolio categories
   const [frontEndOpen, setFrontEndOpen] = useState(true);
   const toggleFrontEnd = () => setFrontEndOpen(prevState => !prevState);
-  const [backEndOpen, setBackEndOpen] = useState(true);
-  const toggleBackEnd = () => setBackEndOpen(prevState => !prevState);
+
+  const [fullStackOpen, setToggleFullStack] = useState(true);
+  const toggleFullStack = () => setToggleFullStack(prevState => !prevState);
+
   const [wordPressOpen, setWordPressOpen] = useState(true);
   const toggleWordPress = () => setWordPressOpen(prevState => !prevState);
 
@@ -23,43 +25,43 @@ function App() {
   const githubLink = "https://github.com/jsaputo1/";
   const email = "jsaputo1@gmail.com";
 
-  const frontEndData = retrieveData(frontEndProjects);
-  const fullStackData = retrieveData(fullStackProjects);
-  const wordPressData = retrieveData(wordPressProjects);
+  //Category header positions
+  const open = "fas fa-chevron-down";
+  const closed = "fas fa-chevron-right";
+
 
   const frontEndJsx =
-    Object.keys(frontEndData).map(function (key) {
+    Object.keys(frontEndProjects).map(function (key) {
       return <Card
-        title={frontEndData[key].title}
-        demoLink={frontEndData[key].demoLink}
-        githubLink={frontEndData[key].githubLink}
-        thumbnail={frontEndData[key].thumbnail}
-        description={frontEndData[key].description}
+        title={frontEndProjects[key].title}
+        demoLink={frontEndProjects[key].demoLink}
+        githubLink={frontEndProjects[key].githubLink}
+        thumbnail={frontEndProjects[key].thumbnail}
+        description={frontEndProjects[key].description}
       />;
     });
 
   const fullStackJsx =
-    Object.keys(fullStackData).map(function (key) {
+    Object.keys(fullStackProjects).map(function (key) {
       return <Card
-        title={fullStackData[key].title}
-        demoLink={fullStackData[key].demoLink}
-        githubLink={fullStackData[key].githubLink}
-        thumbnail={fullStackData[key].thumbnail}
-        description={fullStackData[key].description}
-      />;
-    })
-
-  const wordPressJsx =
-    Object.keys(wordPressData).map(function (key) {
-      return <Card
-        title={wordPressData[key].title}
-        demoLink={wordPressData[key].demoLink}
-        githubLink={wordPressData[key].githubLink}
-        thumbnail={wordPressData[key].thumbnail}
-        description={wordPressData[key].description}
+        title={fullStackProjects[key].title}
+        demoLink={fullStackProjects[key].demoLink}
+        githubLink={fullStackProjects[key].githubLink}
+        thumbnail={fullStackProjects[key].thumbnail}
+        description={fullStackProjects[key].description}
       />;
     });
 
+  const wordPressJsx =
+    Object.keys(wordPressProjects).map(function (key) {
+      return <Card
+        title={wordPressProjects[key].title}
+        demoLink={wordPressProjects[key].demoLink}
+        githubLink={wordPressProjects[key].githubLink}
+        thumbnail={wordPressProjects[key].thumbnail}
+        description={wordPressProjects[key].description}
+      />;
+    });
 
   return (
     <div className="App">
@@ -70,27 +72,60 @@ function App() {
           email={email}
         />
       </div>
-      <Category title="Front End" toggle={toggleFrontEnd} />
-      {frontEndOpen === true ?
-        (<section class="front-end-projects">
-          {frontEndJsx}
-        </section>)
-        : (< section class="front-end-projects" />)
-      }
-      <Category title="Full Stack" toggle={toggleBackEnd} />
-      {backEndOpen === true ?
-        <section class="full-stack-projects">
-          {fullStackJsx}
-        </section>
-        : (<section class="full-stack-projects" />)
-      }
-      <Category title="Word Press" toggle={toggleWordPress} />
-      {wordPressOpen === true ?
-        <section class="word-press-projects">
-          {wordPressJsx}
-        </section>
-        : (<section class="word-press-projects" />)
-      }
+      <section className="front-end-projects">
+        {frontEndOpen === true ?
+          (
+            <div>
+              <Category title="Front End" toggle={toggleFrontEnd} position={open} />
+              <div class="project-container">
+                {frontEndJsx}
+              </div>
+            </div>
+          ) :
+          (
+            <div>
+              <Category title="Front End" toggle={toggleFrontEnd} position={closed} />
+              < section class="front-end-projects" />
+            </div>
+          )
+        }
+      </section>
+      <section className="full-stack-projects">
+        {fullStackOpen === true ?
+          (
+            <div>
+              <Category title="Full Stack" toggle={toggleFullStack} position={open} />
+              <div class="project-container">
+                {fullStackJsx}
+              </div>
+            </div>
+          ) :
+          (
+            <div>
+              <Category title="Full Stack" toggle={toggleFullStack} position={closed} />
+              < section class="full-stack-projects" />
+            </div>
+          )
+        }
+      </section>
+      <section className="word-press-projects">
+        {wordPressOpen === true ?
+          (
+            <div>
+              <Category title="Word Press" toggle={toggleWordPress} position={open} />
+              <div class="project-container">
+                {wordPressJsx}
+              </div>
+            </div>
+          ) :
+          (
+            <div>
+              <Category title="Word Press" toggle={toggleWordPress} position={closed} />
+              < section class="word-press-projects" />
+            </div>
+          )
+        }
+      </section>
     </div>
   );
 
