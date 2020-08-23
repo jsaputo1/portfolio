6e637102f2dda523b3c004f14d1d0b474a2d8a92
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-23 15:20:59 -0400

    added project details for several sites

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,46 +1,86 @@
 const frontEndProjects = {
 
-    s1: {
-      title: "Quotes on Dev",
-      demoLink: "https://www.johnsaputo.net/qod",
-      githubLink: "https://github.com/jsaputo1/quotesondev",
-      thumbnail: "http://localhost:6006/images/thumbnails/qod.png",
-      description: "Quotes on Dev is a multi page web app that shows random developer themed quotes, and also allows registered users to submit their own quotes. The app uses Word Press as the content management system, and is fully responsive with breakpoints for Tablet and Mobile."
-    },
+  s1: {
+    title: "Aloha",
+    demoLink: "https://www.johnsaputo.net/aloha",
+    githubLink: "https://github.com/jsaputo1/aloha",
+    thumbnail: "http://localhost:6006/images/thumbnails/aloha.png",
+    description: "Aloha is a single page design of an online clothing store. It was designed mobile first with responsive views for Tablet and Desktop."
+  },
 
-    s2: {
-      title: "Aloha",
-      demoLink: "https://www.johnsaputo.net/aloha",
-      githubLink: "https://github.com/jsaputo1/aloha",
-      thumbnail: "http://localhost:6006/images/thumbnails/aloha.png",
-      description: "Aloha is a single page design of an online clothing store. It was designed mobile first with responsive views for Tablet and Desktop."
-    }
+  s2: {
+    title: "Instanews",
+    demoLink: "https://www.johnsaputo.net/instanews",
+    githubLink: "https://github.com/jsaputo1/instanews",
+    thumbnail: "http://localhost:6006/images/thumbnails/instanews.png",
+    description: "Instanews is a single page web app that allows users to filter top news stories pulled from the New York Times API. Designed mobile first, it is fully responsive with views for mobile, tablet and desktop."
+  },
+
+  s3: {
+    title: "Fifa Teams",
+    demoLink: "https://www.johnsaputo.net/fifa",
+    githubLink: "#",
+    thumbnail: "http://localhost:6006/images/thumbnails/fifa.png",
+    description: "Built in Spring 2020, this was a fun project to help pass time during the COVID-19 lockdown. This single page site was made to track the custom teams created in a Playstation FIFA 20 league."
+  },
+
+  s4: {
+    title: "Portfolio",
+    demoLink: "https://www.johnsaputo.net/",
+    githubLink: "https://github.com/jsaputo1/portfolio",
+    thumbnail: "http://localhost:6006/images/thumbnails/portfolio.png",
+    description: "The site you're on right now! This site was designed to provide users with my contact information and display projects that I have created"
+  }
 
 };
 
 const fullStackProjects = {
 
   s1: {
-    title: "Quotes on Dev",
-    demoLink: "https://www.johnsaputo.net/qod",
-    githubLink: "https://github.com/jsaputo1/quotesondev",
-    thumbnail: "http://localhost:6006/images/thumbnails/qod.png",
-    description: "Quotes on Dev is a multi page web app that shows random developer themed quotes, and also allows registered users to submit their own quotes. The app uses Word Press as the content management system, and is fully responsive with breakpoints for Tablet and Mobile."
+    title: "Scheduler",
+    demoLink: "https://www.johnsaputo.net/scheduler",
+    githubLink: "https://github.com/jsaputo1/scheduler",
+    thumbnail: "http://localhost:6006/images/thumbnails/scheduler.png",
+    description: "The Interview Scheduler is a single page web app built with React that allows users to book, edit and cancel interviews. The site was deployed with Heroku and Netlify. * Note - The Heroku server auatomatically shuts down after 30 minutes of inactivity. The API data may take 30 seconds to a minute to load. "
   },
 
   s2: {
-    title: "Test",
-    demoLink: "https://www.johnsaputo.net/qod",
-    githubLink: "https://github.com/jsaputo1/quotesondev",
-    thumbnail: "http://localhost:6006/images/thumbnails/qod.png",
-    description: "Test"
-  }
+    title: "Cup O Sugah",
+    demoLink: "https://www.youtube.com/watch?v=ANXvLV38fXI",
+    githubLink: "https://github.com/jsaputo1/neighbourhood",
+    thumbnail: "http://localhost:6006/images/thumbnails/cuposugah.png",
+    description: "CupOSugah is a social media website that allows users to choose a neighbourhood, and only see posts that are made in their neighbourhood. After the user registers their account, the app uses Google Maps geocoding API to determine the user's location, cross references the database and provides 2 of the closest neighbourhoods the user can choose from. *The app is not yet deployed, the link will take you to a video walkthrough on YouTube."
+  },
+
+  s3: {
+    title: "Tweeter",
+    demoLink: "https://www.johnsaputo.net/tweeter",
+    githubLink: "https://github.com/jsaputo1/tweeter",
+    thumbnail: "http://localhost:6006/images/thumbnails/tweeter.png",
+    description: "Tweeter is a simple, single-page Twitter clone where users can post new tweets. The site is responsive with a mobile view. The app was deployed on Heroku."
+  },
+
+  s4: {
+    title: "Tiny App",
+    demoLink: "#",
+    githubLink: "https://github.com/jsaputo1/tinyapp",
+    thumbnail: "http://localhost:6006/images/thumbnails/tinyApp.png",
+    description: "TinyApp is a full stack web application that allows users to shorten long URLs, similar to bit.ly. The app is currently not deployed, please visit the GitHub page for screenshots."
+  },
 
 };
 
 const wordPressProjects = {
 
   s1: {
+    title: "Inhabitent",
+    demoLink: "https://www.johnsaputo.net/inhabitent",
+    githubLink: "https://github.com/jsaputo1/inhabitent",
+    thumbnail: "http://localhost:6006/images/thumbnails/inhabitent.png",
+    description: "Inabitent is a WordPress web store for outdoor/camping equipment. The theme was built from scratch and includes a custom taxonomy to allow the site administrator to add/remove/edit products easily through WordPress without the need to code."
+  },
+
+  s2: {
     title: "Quotes on Dev",
     demoLink: "https://www.johnsaputo.net/qod",
     githubLink: "https://github.com/jsaputo1/quotesondev",
@@ -48,14 +88,15 @@ const wordPressProjects = {
     description: "Quotes on Dev is a multi page web app that shows random developer themed quotes, and also allows registered users to submit their own quotes. The app uses Word Press as the content management system, and is fully responsive with breakpoints for Tablet and Mobile."
   },
 
-  s2: {
-    title: "Test",
-    demoLink: "https://www.johnsaputo.net/qod",
-    githubLink: "https://github.com/jsaputo1/quotesondev",
-    thumbnail: "http://localhost:6006/images/thumbnails/qod.png",
-    description: "Test"
-  }
+  s3: {
+    title: "The League",
+    demoLink: "https://www.johnsaputo.net/the-league",
+    githubLink: "https://github.com/jsaputo1/fifa-league",
+    thumbnail: "http://localhost:6006/images/thumbnails/the-league.png",
+    description: "Built in Spring 2020, this was a fun project to help pass time during the COVID-19 lockdown. This site was created to track scores and standings for a Playstation FIFA 20 league"
+  },
+
 
 };
 
-module.exports = { frontEndProjects};
+module.exports = { frontEndProjects, fullStackProjects, wordPressProjects };
