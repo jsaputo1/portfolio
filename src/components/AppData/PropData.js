1b7e8d3f1e5bbf7d167f4930aa0617964ee92853
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-25 22:02:05 -0400

    moved prop data and global variables from app file to separate files

---
+++ src/components/AppData/PropData.js
@@ -0,0 +1,66 @@
+import React from "react";
+import Card from "../Card/Card";
+const { frontEndProjects, fullStackProjects, wordPressProjects } = require("../../data/projects");
+const { githubLink, email, linkedIn } = require("./Variables");
+
+export const headerProps = {
+  title: "John Saputo",
+  githubLink: githubLink,
+  email: email,
+  linkedIn: linkedIn,
+};
+
+
+export const fullStacProps =
+  Object.keys(fullStackProjects).map(function (key) {
+    return <Card
+      title={fullStackProjects[key].title}
+      demoLink={fullStackProjects[key].demoLink}
+      githubLink={fullStackProjects[key].githubLink}
+      thumbnail={fullStackProjects[key].thumbnail}
+      description={fullStackProjects[key].description}
+      technologies={fullStackProjects[key].technologies}
+      notes={fullStackProjects[key].notes}
+    />;
+  });
+
+export const wordPressProps =
+  Object.keys(wordPressProjects).map(function (key) {
+    return <Card
+      title={wordPressProjects[key].title}
+      demoLink={wordPressProjects[key].demoLink}
+      githubLink={wordPressProjects[key].githubLink}
+      thumbnail={wordPressProjects[key].thumbnail}
+      description={wordPressProjects[key].description}
+      technologies={wordPressProjects[key].technologies}
+      notes={wordPressProjects[key].notes}
+    />;
+  });
+
+export const frontEndProps =
+  Object.keys(frontEndProjects).map(function (key) {
+    return <Card
+      title={frontEndProjects[key].title}
+      demoLink={frontEndProjects[key].demoLink}
+      githubLink={frontEndProjects[key].githubLink}
+      thumbnail={frontEndProjects[key].thumbnail}
+      description={frontEndProjects[key].description}
+      technologies={frontEndProjects[key].technologies}
+      notes={frontEndProjects[key].notes}
+    />;
+  });
+
+export const heroProps = {
+  title: "John Saputo",
+  tag: "Web Developer",
+  gitHubLink: githubLink,
+  email: email,
+  article: " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sit amet ante ante. Nulla facilisi. Proinvitae libero sapien. Mauris at pretium ipsum, at eleifend velit. Donec lorem dui, tempus ac dignissim eget, porta et justo. Praesent eget laoreet elit, nec congue ante..",
+  image: "/images/hero-banner3.png",
+};
+
+
+export const sliderSettings = {
+  slidesToShow: 3,
+  dots: true
+};
---
+++ src/components/AppData/Variables.js
@@ -0,0 +1,7 @@
+export const githubLink = "https://github.com/jsaputo1/";
+export const email = "jsaputo1@gmail.com";
+export const linkedIn = "https://www.linkedin.com/in/john-saputo-09a53b7b/";
+
+//Category header positions
+export const open = "fas fa-chevron-up";
+export const closed = "fas fa-chevron-down";
