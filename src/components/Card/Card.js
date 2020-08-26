e55ad459f02b8f8c1e1a552453ed3f98af5f33ef
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-25 21:26:46 -0400

    changed description to include technologies and notes

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -10,15 +10,20 @@ function Card(props) {
       <a href={props.demoLink} target="new">
         <img src={props.thumbnail} alt="website-preview" />
       </a>
-      <div className="card-description-header">
-        <h3>Description</h3>
+      <div className="card-information-header">
+        <h3>Information</h3>
         <div className="website-links">
           <span><a href={props.githubLink} target="new"><i class="fab fa-github"></i></a></span>
           <span><a href={props.demoLink} target="new"><i class="fas fa-external-link-alt"></i></a></span>
         </div>
       </div>
-      <div className="card-description">
-        <h4>{props.description}</h4>
+      <div className="card-information-details">
+        <span><h3>Description:</h3></span>
+        <span><h4>{props.description}</h4></span>
+        <span><h3>Technologies:</h3></span>
+        <span><h4>{props.technologies}</h4></span>
+        <span><h3>Notes:</h3></span>
+        <span><h4>{props.notes}</h4></span>
       </div>
     </figure>
   </div>;
