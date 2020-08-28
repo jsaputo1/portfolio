42b660f63b5b303edcb351cc2419e7920fee1fd5
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-27 20:53:55 -0400

    added link to index to left-side header text

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ function Header(props) {
 
   return <header className="header-container">
     <div className="left-side-header">
-      <h3>{props.title}</h3>
+      <a href="/index"><h3>{props.title}</h3></a>
     </div>
     <div className="left-side-header-mobile">
       <Dropdown isOpen={dropdownOpen} toggle={toggle}>
