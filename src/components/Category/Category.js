acf37a330976200e7999ca30f76a2b1a2ae49096
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-09-08 17:19:27 -0400

    added useApplicationHook and code for list for categories

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import useApplicationData from "../hooks/useApplicationData";
 import "./Category.scss";
 
 function Category(props) {
-  return <div className="category-header">
-    <hr />
-    <a href={props.idLink}> <h3 onClick={props.toggle}>{props.title}</h3></a>
-    <hr />
-    <a href={props.idLink}><i class={props.position} onClick={props.toggle}></i></a>
+
+  console.log("Category props", props)
+
+  const {
+    state,
+    setCategory
+  } = useApplicationData();
+
+  console.log("Category state", state)
+
+  return <div>
+    <li className=
+      {state.category !== props.name ? "nav-list-item" : "nav-list-item active"}
+      onClick={() => {
+        props.toggleOff();
+        props.toggleCategory();
+        setCategory(props.name)
+      }}>
+      {props.name}</li>
   </div>;
 }
 
