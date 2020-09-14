fc465b6bc60828e2f901c518893ebdfef61b3a2f
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-09-14 16:32:54 -0400

    added sticky hook

--- src/components/hooks/useApplicationData.js
+++ src/components/hooks/useApplicationData.js
@@ -4,13 +4,16 @@ export default function useApplicationData() {
 
   const [state, setState] = useState({
     category: "default",
+    sticky: false
   });
 
   const setCategory = (category) => setState({ ...state, category });
+  const setSticky = (sticky) => setState({ ...state, sticky });
 
   return {
     state,
-    setCategory
+    setCategory,
+    setSticky
   };
 
 }
