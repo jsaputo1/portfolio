16f9c6572a2a53077647f5960ed26e75fa1fc492
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-09-14 20:37:29 -0400

    removed divs for animated bg

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,17 +3,6 @@ import "./Hero.scss";
 
 function Hero(props) {
   return <div className="hero-container">
-    <div class="waveWrapper waveAnimation">
-      <div class="waveWrapperInner bgTop">
-        <div class="wave waveTop"></div>
-      </div>
-      <div class="waveWrapperInner bgMiddle">
-        <div class="wave waveMiddle"></div>
-      </div>
-      <div class="waveWrapperInner bgBottom">
-        <div class="wave waveBottom"></div>
-      </div>
-    </div>
     <div className="hero-text">
       <h1>Hello, I'm <span className="hero-name">{props.name}</span></h1>
       <h1>{props.tagline}</h1>
