c2122ec42aaf05d657d9356248b587fd35fa6687
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-09-17 18:16:55 -0400

    removed dropdown menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Header.scss";
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 function Header(props) {
-
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-  const toggle = () => setDropdownOpen(prevState => !prevState);
 
   window.onscroll = function () {
     console.log(window.pageYOffset);
@@ -20,18 +16,6 @@ function Header(props) {
   return <header className={props.sticky ? "header-container sticky slide-down" : "header-container"}>
     <div className="left-side-header">
       <a href={props.headerLink}><h3>{props.title}</h3></a>
-    </div>
-    <div className="left-side-header-mobile">
-      <Dropdown isOpen={dropdownOpen} toggle={toggle}>
-        <DropdownToggle>
-          <i class="fas fa-bars"></i>
-        </DropdownToggle>
-        <DropdownMenu>
-          <DropdownItem><a href="#full-stack" onClick={() => { props.openFullStack(); }}>Full Stack</a></DropdownItem>
-          <DropdownItem ><a href="#word-press" onClick={() => { props.openWordPress(); }}>Word Press</a></DropdownItem>
-          <DropdownItem><a href="#front-end" onClick={() => { props.openFrontEnd(); }}>Front End</a></DropdownItem>
-        </DropdownMenu>
-      </Dropdown>
     </div>
     <div className="right-side-header">
       <span><a href={props.githubLink} target="new"><i class="fab fa-github"></i></a></span>
